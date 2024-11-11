7ae6bcbb5a7b72d3777568da888283a386c3f10c
Jake Kealey <jake.kealey@nhs.net>
2024-11-11 09:19:51 +0000

    CREATE TABLE SQL for the main sickness data

---
+++ docs/create_table_sickness.sql
@@ -0,0 +1,23 @@
+--CREATE TABLE STATEMENT--
+--For the main sickness data table
+CREATE TABLE [Data_Lab_NCL_Dev].[JakeK].[wf_sickness] (
+    --Date columns
+    date_extract DATETIME NOT NULL,
+    date_data DATE NOT NULL,
+
+    --ICS + Organisation Columns
+    ics_code CHAR(3) NOT NULL,
+    ics_name VARCHAR(80) NOT NULL,
+    org_code CHAR(3) NOT NULL,
+    org_name VARCHAR(80) NOT NULL,
+
+    --Breakdown Columns
+    staffgroup VARCHAR(60) NOT NULL,
+
+    --Metric Numerator and Denominator
+    days_lost FLOAT,
+    days_available FLOAT
+
+    --Primary Key Restriction
+    PRIMARY KEY (date_data, org_code, staffgroup)
+);
