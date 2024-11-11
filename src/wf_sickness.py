ab0870ea62a91929109ac72d91e10e152b11bfcc
Jake Kealey <jake.kealey@nhs.net>
2024-11-11 10:18:20 +0000

    Added functionality to upload processed data to the database destination. Added functionality to derive delete query when attempting to reupload data.

--- src/wf_sickness.py
+++ src/wf_sickness.py
@@ -5,9 +5,12 @@ import toml
 from datetime import datetime
 from dotenv import load_dotenv
 from os import getenv
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
 
 #Functions
 
+#Return an object containing all runtime settings
 def load_settings():
     #Load env settings
     load_dotenv(override=True)
@@ -17,6 +20,7 @@ def load_settings():
 
     settings = {
 
+        "sql_address": getenv("SQL_ADDRESS"),
         "sql_database": config["database"]["sql_database"],
         "sql_schema": config["database"]["sql_schema"],
         "sql_table_sickness": config["database"]["sql_table_sickness"],
@@ -29,6 +33,19 @@ def load_settings():
 
     return settings
 
+#Establish a connection to the database
+def db_connect(server_address, database, 
+               server_type="mssql", driver="SQL+Server"):
+    
+    #Create Connection String
+    conn_str = (f"{server_type}://{server_address}/{database}"
+                f"?trusted_connection=yes&driver={driver}")
+    
+    #Create SQL Alchemy Engine object
+    engine = create_engine(conn_str, use_setinputsizes=False)
+    return engine
+
+#Return a list of all csv files in the data/current directory
 def get_source_files():
     #Get all files in the source data directory
     data_dir = "./data/current/"
@@ -77,9 +94,51 @@ def process_benchmarking_data(df_in, settings):
     df.drop("region_code", axis=1, inplace=True)
 
     #Add a current timestamp to the data
-    df["date_extract"] = datetime.today()
+    df["date_upload"] = datetime.today()
+
+    #Convert existing date column to date object
+    df["date_data"] = pd.to_datetime(df["date_data"], dayfirst=True)
     
     return df
+
+#Function to upload data for a given dataset
+def upload_data(df, dataset, settings):
+
+    #Load destination table name
+    try:
+        sql_table = settings["sql_table_" + dataset]
+    except:
+        raise Exception((f"'sql_table_{dataset}' was not found in the"
+                          "'[database]' section of the config.toml file."))
+
+    #Set up Database Connection
+    server_address = settings["sql_address"]
+    sql_database = settings["sql_database"]
+
+    #Connect to the database
+    engine = db_connect(server_address, sql_database)
+
+    #Upload the processed data
+    sql_schema = settings["sql_schema"]
+    
+    ##Delete existing overlapping data to allow for re-uploading
+    data_daterange = df["date_data"].unique()
+    if len(data_daterange) > 1:
+        raise Warning(("Multiple dates were found in the data.\n"
+                      "This process does not replace existing data in the "
+                      "destination for files with multiple dates."))
+    
+    if len(data_daterange) == 1:
+        del_query = (f"DELETE FROM "
+                     f"[{sql_database}].[{sql_schema}].[{sql_table}] "
+                     f"WHERE date_data = '{data_daterange[0]}'")
+        
+
+    ##Upload the processed data
+    df.to_sql(sql_table, engine, schema=sql_schema, 
+              if_exists="append", index=False, method="multi",
+              chunksize=200)
+
 
 #Load the runtime settings
 settings = load_settings()
@@ -96,6 +155,5 @@ for sf in source_files:
     #Transform the data
     df_processed = process_benchmarking_data(df_source, settings)
 
-    print(df_processed.head())
-
     #Load the data into the warehouse
+    upload_data(df_processed, "sickness", settings)
