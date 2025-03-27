47db3193c1bd97fddd994a172d5a59118ed77f6b
Jake Kealey <jake.kealey@nhs.net>
2025-03-27 15:33:22 +0000

    Updated tables to account for NLFT having a 5 digit org code and not properly appearing in the dictionary table

--- docs/create_table_sickness_byreason.sql
+++ docs/create_table_sickness_byreason.sql
@@ -5,9 +5,9 @@ CREATE TABLE [Data_Lab_NCL_Dev].[JakeK].[wf_sickness_byreason] (
     date_data DATE NOT NULL,
 
     --ICS + Organisation Columns
-    ics_code CHAR(3) NOT NULL,
+    ics_code VARCHAR(5) NOT NULL,
     ics_name VARCHAR(80) NOT NULL,
-    org_code CHAR(3) NOT NULL,
+    org_code VARCHAR(5) NOT NULL,
     org_name VARCHAR(80) NOT NULL,
 
     --Breakdown Columns
